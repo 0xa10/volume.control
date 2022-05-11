1ecf5eb417d721edbb4176a622f95680874957c0
Alon L <alon@botanica.consulting>
2022-05-11 20:23:39 +0300

    Working serial/interrupt code

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,132 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
+// Rust Embedded stuff
 use cortex_m_rt::entry;
+use embedded_time::fixed_point::FixedPoint;
+use embedded_hal::digital::v2::OutputPin;
+
+// Defmt is a microcontroller oriented formatting library
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use embedded_time::fixed_point::FixedPoint;
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
+use rp_pico::hal as hal;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+use hal::pac::interrupt;
+use hal::clocks::Clock;
+
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
+use hal::usb::UsbBus;
+use usbd_serial::SerialPort;
+
+
+// Global USB objects
+static mut USB_DEVICE: Option<UsbDevice<hal::usb::UsbBus>> = None;
+static mut USB_BUS: Option<UsbBusAllocator<hal::usb::UsbBus>> = None;
+static mut USB_SERIAL: Option<SerialPort<hal::usb::UsbBus>> = None;
+
 
 #[entry]
 fn main() -> ! {
-    info!("Program start");
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+	//
+	// Setup
+	//
+    let mut pac = hal::pac::Peripherals::take().unwrap(); // Todo - find a better pattern than unwrap, very unrustlike
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+    let sio = hal::sio::Sio::new(pac.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+	let xtal_freq_hz = rp_pico::XOSC_CRYSTAL_FREQ;
+
+    let clocks = hal::clocks::init_clocks_and_plls(
+		xtal_freq_hz,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
         pac.PLL_USB,
         &mut pac.RESETS,
         &mut watchdog,
-    )
-    .ok()
-    .unwrap();
+    ).ok().unwrap();
 
+
+	//
+	// Auxiliary objects
+	//
+    let core = hal::pac::CorePeripherals::take().unwrap();
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
 
-    let pins = bsp::Pins::new(
+	// Pin setup
+    let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-
     let mut led_pin = pins.led.into_push_pull_output();
 
+	//
+	// USB setup, bus, device, and driver
+	
+	let usb_bus = UsbBusAllocator::new(UsbBus::new(
+				pac.USBCTRL_REGS,
+				pac.USBCTRL_DPRAM,
+				clocks.usb_clock,
+				true,
+				&mut pac.RESETS,
+				));
+	unsafe {
+        // Set the global USB_BUS object
+        USB_BUS = Some(usb_bus);
+    }
+	let usb_bus_reference = unsafe { USB_BUS.as_ref().unwrap() }; // Not quite sure why this is needed, perhaps the old ref is moved?
+
+	// Set up the driver
+    let usb_serial = SerialPort::new(usb_bus_reference);
+	unsafe {
+		USB_SERIAL = Some(usb_serial);
+	}
+
+    let usb_device = UsbDeviceBuilder::new(usb_bus_reference, UsbVidPid(0x1337, 0xb00b))
+        .manufacturer("Pini")
+        .product("Grigioer")
+        .serial_number("1234")
+        .device_class(2) // from: https://www.usb.org/defined-class-codes
+        .build();
+	unsafe {
+        // Set the global USB_BUS object
+        USB_DEVICE = Some(usb_device);
+    }
+
+	//
+	// Main loop 
+	//
+	// We should unmask the relevant interrupts now, and avoid sync issues.
+	debug!("unmasking interrupts.");
+	// USB
+	unsafe {
+        hal::pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    };
+	
+	debug!("main loop starting.");
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(100);
-        info!("off!");
+		cortex_m::interrupt::free(|_| unsafe {
+			USB_SERIAL.as_mut().unwrap().write(b"hellosup\r\n")
+		}).unwrap();
+		 led_pin.set_high().unwrap();
+        delay.delay_ms(500);
         led_pin.set_low().unwrap();
-        delay.delay_ms(100);
+        delay.delay_ms(500);
     }
 }
 
-// End of file
+// USB Interrupt handler
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    let usb_device = USB_DEVICE.as_mut().unwrap();
+    let usb_serial = USB_SERIAL.as_mut().unwrap();
+    usb_device.poll(&mut [usb_serial]);
+}
+
