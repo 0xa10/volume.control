f368dfda63d1e190ff1a7992448e7911db3cdcd7
Alon <alonlivne@gmail.com>
2022-06-27 17:23:08 +0300

    Transitioned to custom HID report using different library. Still a mixed sync/async interrupt driven model

---
+++ src/hid.rs
@@ -0,0 +1,99 @@
+use delegate::delegate;
+use embedded_time::duration::Milliseconds;
+use packed_struct::prelude::*;
+use usb_device::bus::{InterfaceNumber, StringIndex, UsbBus};
+use usb_device::class_prelude::DescriptorWriter;
+
+use usbd_human_interface_device::hid_class::descriptor::HidProtocol;
+use usbd_human_interface_device::hid_class::prelude::*;
+use usbd_human_interface_device::interface::raw::{RawInterface, RawInterfaceConfig};
+use usbd_human_interface_device::interface::{
+    InterfaceClass, WrappedInterface, WrappedInterfaceConfig,
+};
+use usbd_human_interface_device::UsbHidError;
+
+#[rustfmt::skip]
+pub const VOLUME_CONTROL_DESCRIPTOR: &[u8] = &[
+    0x05, 0x0C, //        Usage Page (Consumer Devices)  
+    0x09, 0x01, //        Usage (Consumer Control)  
+    0xA1, 0x01, //        Collection (Application)  
+    0x05, 0x0C, //            Usage Page (Consumer Devices)  
+    0x15, 0x00, //            Logical Minimum (0)  
+    0x25, 0x01, //            Logical Maximum (1)  
+    0x75, 0x01, //            Report Size (1)  
+    0x95, 0x07, //            Report Count (3)  
+    0x09, 0xE2, //            Usage (Mute)  
+    0x09, 0xE9, //            Usage (Volume Increment)  
+    0x09, 0xEA, //            Usage (Volume Decrement)  
+    0x81, 0x02, //            Input (Data,Var,Abs,NWrp,Lin,Pref,NNul,Bit)  
+    0x95, 0x01, //            Report Count (5)  
+    0x81, 0x01, //            Input (Const,Ary,Abs)  
+    0xC0, //        End Collection
+];
+
+#[derive(Clone, Copy, Debug, PartialEq, Default, PackedStruct)]
+#[packed_struct(endian = "lsb", bit_numbering = "lsb0", size_bytes = "1")]
+pub struct VolumeControl {
+    #[packed_field(bits = "0")]
+    pub mute: bool,
+    #[packed_field(bits = "1")]
+    pub volume_increment: bool,
+    #[packed_field(bits = "2")]
+    pub volume_decrement: bool,
+}
+
+pub struct VolumeControlInterface<'a, B: UsbBus> {
+    inner: RawInterface<'a, B>,
+}
+
+impl<'a, B: UsbBus> VolumeControlInterface<'a, B> {
+    pub fn write_report(&self, report: &VolumeControl) -> Result<(), UsbHidError> {
+        // Explicitly map error to SerializationError
+        let data = report.pack().map_err(|_| UsbHidError::SerializationError)?;
+
+        self.inner
+            .write_report(&data)
+            .map_err(UsbHidError::from)
+            .map(|_| ())
+    }
+
+    pub fn default_config() -> WrappedInterfaceConfig<Self, RawInterfaceConfig<'a>> {
+        WrappedInterfaceConfig::new(
+            RawInterfaceBuilder::new(VOLUME_CONTROL_DESCRIPTOR)
+                .description("volume.control")
+                .idle_default(Milliseconds(0))
+                .unwrap()
+                .in_endpoint(UsbPacketSize::Bytes8, Milliseconds(10))
+                .unwrap()
+                .build(),
+            (),
+        )
+    }
+}
+
+impl<'a, B: UsbBus> InterfaceClass<'a> for VolumeControlInterface<'a, B> {
+    delegate! {
+        to self.inner{
+           fn report_descriptor(&self) -> &'_ [u8];
+           fn id(&self) -> InterfaceNumber;
+           fn write_descriptors(&self, writer: &mut DescriptorWriter) -> usb_device::Result<()>;
+           fn get_string(&self, index: StringIndex, _lang_id: u16) -> Option<&'_ str>;
+           fn reset(&mut self);
+           fn set_report(&mut self, data: &[u8]) -> usb_device::Result<()>;
+           fn get_report(&mut self, data: &mut [u8]) -> usb_device::Result<usize>;
+           fn get_report_ack(&mut self) -> usb_device::Result<()>;
+           fn set_idle(&mut self, report_id: u8, value: u8);
+           fn get_idle(&self, report_id: u8) -> u8;
+           fn set_protocol(&mut self, protocol: HidProtocol);
+           fn get_protocol(&self) -> HidProtocol;
+        }
+    }
+}
+
+impl<'a, B: UsbBus> WrappedInterface<'a, B, RawInterface<'a, B>>
+    for VolumeControlInterface<'a, B>
+{
+    fn new(interface: RawInterface<'a, B>, _: ()) -> Self {
+        Self { inner: interface }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 use defmt_rtt as _;
 use panic_probe as _;
 
+mod hid;
+
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [XIP_IRQ])]
 mod app {
     use core::mem::MaybeUninit;
@@ -16,22 +18,17 @@ mod app {
     use rp_pico::hal;
     use rp_pico::XOSC_CRYSTAL_FREQ;
 
-	use hal::Clock;
     use hal::usb::UsbBus;
     use usb_device::{class_prelude::*, prelude::*};
-
-	use usbd_human_interface_device::device::consumer::{ConsumerControlInterface, MultipleConsumerReport};
-	use usbd_human_interface_device::device::keyboard::NKROBootKeyboardInterface;
-
-	use usbd_human_interface_device::prelude::*;
-
+    
+    use usbd_human_interface_device::hid_class::prelude::*;
+    use crate::hid::{VolumeControlInterface, VolumeControl};
+    
 	use frunk::HList;
-
-	use usbd_human_interface_device::page::Consumer as ConsumerPage;
 
     use rotary_encoder_hal::{Direction, Rotary};
 
-    use heapless::spsc::{Consumer as QueueConsumer, Producer, Queue};
+    use heapless::spsc::{Consumer as Consumer, Producer, Queue};
 
     #[monotonic(binds = TIMER_IRQ_0, default = true)]
     type Monotonic = Rp2040Monotonic;
@@ -42,13 +39,10 @@ mod app {
 
     #[shared]
     struct Shared {
-        usb_hid: UsbHidClass<UsbBus, HList!(
-			ConsumerControlInterface<'static, hal::usb::UsbBus>,
-			NKROBootKeyboardInterface<'static, hal::usb::UsbBus, SyncTimerClock>,
-		)>,
+        usb_hid: UsbHidClass<UsbBus, HList!(VolumeControlInterface<'static, UsbBus>,)>,
         usb_device: UsbDevice<'static, UsbBus>,
-        hid_consumer: QueueConsumer<'static, MultipleConsumerReport, 4>,
-        hid_producer: Producer<'static, MultipleConsumerReport, 4>,
+        hid_consumer: Consumer<'static, VolumeControl, 4>,
+        hid_producer: Producer<'static, VolumeControl, 4>,
         switch_pin: SwitchPin,
         rotary_encoder: Rotary<DTPin, CLKPin>,
     }
@@ -60,7 +54,7 @@ mod app {
 
     #[init(local = [
 			usb_bus: MaybeUninit<UsbBusAllocator<UsbBus>> = MaybeUninit::uninit(),
-			hid_queue: Queue<MultipleConsumerReport, 4> = Queue::new(),
+			hid_queue: Queue<VolumeControl, 4> = Queue::new(),
 		])]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         info!("Starting init");
@@ -98,6 +92,7 @@ mod app {
         rotary_clk.set_interrupt_enabled(hal::gpio::Interrupt::EdgeLow, true);
         rotary_clk.set_interrupt_enabled(hal::gpio::Interrupt::EdgeHigh, true);
         let rotary_encoder = Rotary::new(rotary_dt, rotary_clk);
+
         let switch_pin = pins.gpio2.into_mode();
         if let Ok(switch_pin_low_on_init) = switch_pin.is_low() {
             if switch_pin_low_on_init {
@@ -105,6 +100,7 @@ mod app {
             }
         }
         switch_pin.set_interrupt_enabled(hal::gpio::Interrupt::EdgeLow, true);
+
         let usb_bus: &'static _ = cx.local.usb_bus.write(UsbBusAllocator::new(UsbBus::new(
             cx.device.USBCTRL_REGS,
             cx.device.USBCTRL_DPRAM,
@@ -113,16 +109,9 @@ mod app {
             &mut resets,
         )));
 
-		let clock = SyncTimerClock::new(hal::Timer::new(
-            cx.device.TIMER,
-            &mut resets));
         let usb_hid = UsbHidClassBuilder::new()
-				.add_interface(
-					NKROBootKeyboardInterface::default_config(clock),
-				)
-				.add_interface(
-					ConsumerControlInterface::default_config(),
-				).build(&usb_bus);
+            .add_interface(VolumeControlInterface::default_config())
+            .build(usb_bus);
 
         let usb_device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x1209, 0x4853))
             .manufacturer("pini.grigio")
@@ -171,13 +160,13 @@ mod app {
     #[task(binds = IO_IRQ_BANK0, priority = 2, shared = [rotary_encoder, switch_pin])]
     fn on_gpio(cx: on_gpio::Context) {
         // We skip checking the interrupt origin and simply clear all our GPIO interrupts,
-        // and trigger both rotation and click events.
+        // and trigger a HID report whenever the pin state changes.
         // Checking the origin is more logically sound, but it does not save a considerable amount of
         // cycles while complicating the code.
         (cx.shared.switch_pin, cx.shared.rotary_encoder).lock(|switch_pin, rotary_encoder| {
             // Clear switch event and dispatch handler
             switch_pin.clear_interrupt(hal::gpio::Interrupt::EdgeLow);
-            process_switch_event::spawn_after(800.micros()).ok(); // Small debounce delay
+            process_switch_event::spawn_after(2.millis()).ok(); 
 
             // Clear rotary event and dispatch handler
             let pins = rotary_encoder.pins();
@@ -187,7 +176,8 @@ mod app {
             pins.1.clear_interrupt(hal::gpio::Interrupt::EdgeHigh);
             pins.1.clear_interrupt(hal::gpio::Interrupt::EdgeLow);
 
-            process_rotation_event::spawn().ok();
+            // 2ms debouncing delay - meaning we check only after the switch has settled.
+            process_rotation_event::spawn_after(1.micros()).ok(); 
         });
     }
     #[task(binds = USBCTRL_IRQ, priority = 3, shared = [usb_device, usb_hid])]
@@ -200,49 +190,49 @@ mod app {
 
     #[task(shared = [switch_pin, hid_producer])]
     fn process_switch_event(cx: process_switch_event::Context) {
-        let mut switch_pin = cx.shared.switch_pin;
-        let mut hid_producer = cx.shared.hid_producer;
-        if let Ok(switch_pin_state) = switch_pin.lock(|sp| sp.is_low()) {
-            if switch_pin_state {
-				hid_producer.lock(|p| p.enqueue(MultipleConsumerReport {
-					codes: [ConsumerPage::Mute, ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned]
-				}).ok());
+        (cx.shared.switch_pin, cx.shared.hid_producer).lock(
+            |switch_pin, hid_producer| {
+                // Assemble volume control report
+                let switch_pin_state = switch_pin.is_low().unwrap_or(false);
+                
+                if switch_pin_state {
+                    hid_producer.enqueue(VolumeControl {
+                        mute: true,
+                        volume_increment: false,
+                        volume_decrement: false,
+                    }).ok();
+
+                    hid_producer.enqueue(VolumeControl {mute: false, volume_increment: false, volume_decrement: false }).ok();
+                }
             }
-        }
+        );
     }
 
     #[task(shared = [rotary_encoder, hid_producer])]
     fn process_rotation_event(cx: process_rotation_event::Context) {
-        let mut rotary_encoder = cx.shared.rotary_encoder;
-        let mut hid_producer = cx.shared.hid_producer;
-        if let Ok(direction) = rotary_encoder.lock(|re| re.update()) {
-            match direction {
-                Direction::Clockwise => {
-                    hid_producer.lock(|p| p.enqueue(MultipleConsumerReport {
-						codes: [ConsumerPage::VolumeIncrement, ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned]
-					}).ok());
-                    hid_producer.lock(|p| p.enqueue(MultipleConsumerReport {
-						codes: [ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned]
-					}).ok());
-                }
-                Direction::CounterClockwise => {
-                    hid_producer.lock(|p| p.enqueue(MultipleConsumerReport {
-						codes: [ConsumerPage::VolumeDecrement, ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned]
-					}).ok());
-                    hid_producer.lock(|p| p.enqueue(MultipleConsumerReport {
-						codes: [ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned, ConsumerPage::Unassigned]
-					}).ok());
-                }
-                Direction::None => {}
+        (cx.shared.rotary_encoder, cx.shared.hid_producer).lock(
+            |rotary_encoder, hid_producer| {
+                // Assemble volume control report
+                let direction = rotary_encoder.update().unwrap_or(Direction::None);
+                
+                hid_producer.enqueue(VolumeControl {
+                    mute: false,
+                    volume_increment: direction == Direction::Clockwise,
+                    volume_decrement: direction == Direction::CounterClockwise,
+                }).ok();
+                hid_producer.enqueue(VolumeControl {mute: false, volume_increment: false, volume_decrement: false }).ok();
+
             }
-        }
+        );
     }
 
     #[task(shared = [usb_device, usb_hid, hid_consumer])]
     fn usb_hid_task(mut cx: usb_hid_task::Context) {
+        // This task is responsible for emitting HID events in accordance
+        // with the USB polling interval.
+        // A limited backlog of events may be queued.
         let mut hid_consumer = cx.shared.hid_consumer;
 
-        // Do a single event every poll interval.
         if let Some(report) = hid_consumer.lock(|h| h.dequeue()) {
             // Event was queued by encoder task, send to USB.
             debug!(
